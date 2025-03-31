5bfe864ce7934a691d49e94035e7be5ab3145f7d
Alvin Cheng <eventide1029@gmail.com>
2025-03-31 12:02:29 +1100

    v2

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,62 +22,33 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        txt = New TextBox()
-        Label1 = New Label()
         Button1 = New Button()
-        Button2 = New Button()
-        Button3 = New Button()
         Button4 = New Button()
         Button5 = New Button()
-        res = New Label()
+        test2 = New TextBox()
+        test1 = New TextBox()
+        surname = New TextBox()
+        title = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
         SuspendLayout()
-        ' 
-        ' txt
-        ' 
-        txt.Location = New Point(80, 107)
-        txt.Name = "txt"
-        txt.Size = New Size(200, 38)
-        txt.TabIndex = 0
-        ' 
-        ' Label1
-        ' 
-        Label1.AutoSize = True
-        Label1.Location = New Point(80, 209)
-        Label1.Name = "Label1"
-        Label1.Size = New Size(129, 31)
-        Label1.TabIndex = 1
-        Label1.Text = "Check for:"
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(460, 277)
+        Button1.Location = New Point(59, 665)
         Button1.Name = "Button1"
         Button1.Size = New Size(150, 46)
         Button1.TabIndex = 2
-        Button1.Text = "Yes / No"
+        Button1.Text = "Validate"
         Button1.UseVisualStyleBackColor = True
-        ' 
-        ' Button2
-        ' 
-        Button2.Location = New Point(270, 277)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(150, 46)
-        Button2.TabIndex = 3
-        Button2.Text = "Number"
-        Button2.UseVisualStyleBackColor = True
-        ' 
-        ' Button3
-        ' 
-        Button3.Location = New Point(80, 277)
-        Button3.Name = "Button3"
-        Button3.Size = New Size(150, 46)
-        Button3.TabIndex = 4
-        Button3.Text = "Postcode"
-        Button3.UseVisualStyleBackColor = True
         ' 
         ' Button4
         ' 
-        Button4.Location = New Point(769, 392)
+        Button4.Location = New Point(767, 655)
         Button4.Name = "Button4"
         Button4.Size = New Size(150, 46)
         Button4.TabIndex = 5
@@ -86,48 +57,130 @@ Partial Class Form1
         ' 
         ' Button5
         ' 
-        Button5.Location = New Point(591, 392)
+        Button5.Location = New Point(577, 655)
         Button5.Name = "Button5"
         Button5.Size = New Size(150, 46)
         Button5.TabIndex = 7
         Button5.Text = "Clear"
         Button5.UseVisualStyleBackColor = True
         ' 
-        ' res
+        ' test2
         ' 
-        res.AutoSize = True
-        res.Location = New Point(515, 142)
-        res.Name = "res"
-        res.Size = New Size(24, 31)
-        res.TabIndex = 8
-        res.Text = "-"
+        test2.Location = New Point(496, 359)
+        test2.Name = "test2"
+        test2.Size = New Size(336, 38)
+        test2.TabIndex = 0
+        ' 
+        ' test1
+        ' 
+        test1.Location = New Point(496, 315)
+        test1.Name = "test1"
+        test1.Size = New Size(336, 38)
+        test1.TabIndex = 8
+        ' 
+        ' surname
+        ' 
+        surname.Location = New Point(496, 241)
+        surname.Name = "surname"
+        surname.Size = New Size(336, 38)
+        surname.TabIndex = 9
+        ' 
+        ' title
+        ' 
+        title.Location = New Point(496, 197)
+        title.Name = "title"
+        title.Size = New Size(336, 38)
+        title.TabIndex = 10
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(126, 362)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(83, 31)
+        Label1.TabIndex = 11
+        Label1.Text = "Test 2"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(126, 318)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(83, 31)
+        Label2.TabIndex = 12
+        Label2.Text = "Test 1"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(126, 237)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(116, 31)
+        Label3.TabIndex = 13
+        Label3.Text = "Surname"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(126, 193)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(285, 31)
+        Label4.TabIndex = 14
+        Label4.Text = "Title (Mr, Miss, Ms, Mrs)"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(316, 73)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(271, 31)
+        Label5.TabIndex = 15
+        Label5.Text = "Students Results Entry"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(276, 524)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(24, 31)
+        Label6.TabIndex = 16
+        Label6.Text = "-"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(14F, 31F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(948, 472)
-        Controls.Add(res)
+        ClientSize = New Size(961, 735)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(title)
+        Controls.Add(surname)
+        Controls.Add(test1)
         Controls.Add(Button5)
         Controls.Add(Button4)
-        Controls.Add(Button3)
-        Controls.Add(Button2)
         Controls.Add(Button1)
-        Controls.Add(Label1)
-        Controls.Add(txt)
+        Controls.Add(test2)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
         PerformLayout()
     End Sub
-
-    Friend WithEvents txt As TextBox
-    Friend WithEvents Label1 As Label
     Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
     Friend WithEvents Button5 As Button
-    Friend WithEvents res As Label
+    Friend WithEvents test2 As TextBox
+    Friend WithEvents test1 As TextBox
+    Friend WithEvents surname As TextBox
+    Friend WithEvents title As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -3,28 +3,36 @@
         End
     End Sub
 
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        Check(Val(txt.Text) <= 2999 And Val(txt.Text) > 3999)
-    End Sub
+    Function Assert(condition As Boolean, msg As String) As Boolean
+        If condition = False Then
+            MsgBox(msg)
+            Return False
+        End If
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Check(IsNumeric(txt.Text))
-    End Sub
+        Return True
+    End Function
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Check(LCase(txt.Text) = "yes" Or LCase(txt.Text) = "no")
+        Dim resTitle As String = LCase(title.Text)
+        Dim okay As Boolean = False
+        okay = Assert(resTitle = "mr" Or resTitle = "ms" Or resTitle = "miss" Or resTitle = "mrs",
+                       "The title is incorrect, it must be either Mr, Miss, Mrs or Ms!")
+        okay = Assert(Val(Len(surname.Text)) > 0 And Val(Len(surname.Text)) < 15,
+                        "Your surname should be at least 1 character long and no more than 15!")
+        okay = Assert((Val(test1.Text) + Val(test2.Text)) < 200, "The sum should be less than 200")
+        okay = Assert(Val(test1.Text) >= 0 And Val(test1.Text) <= 100, "The first test should be between 0 and 100")
+        okay = Assert(Val(test2.Text) >= 0 And Val(test2.Text) <= 100, "The second test should be between 0 and 100")
+
+        If okay Then
+            Label6.Text = "The student " + title.Text + " " + surname.Text + " has been validated!"
+        End If
     End Sub
 
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
-        txt.Text = ""
-        res.Text = "-"
+        title.Text = ""
+        surname.Text = ""
+        test1.Text = ""
+        test2.Text = ""
+        Label6.Text = "-"
     End Sub
-
-    Function Check(condition As Boolean)
-        If condition = True Then
-            res.Text = "✅"
-        Else
-            res.Text = "❌"
-        End If
-    End Function
 End Class
